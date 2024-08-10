ad6f458336f3e4e8c6be2cbd9dca0211ab8d1dcc
verylowpower <doudqwer@gmail.com>
2024-08-10 15:21:33 +0700

    thêm phương thức di chuyển

--- Assets/Code/Move.cs
+++ Assets/Code/Move.cs
@@ -3,16 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Move : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+{	
+	[SerializeField] float speed = 2.5f;
+	public Rigidbody2D body;
+	void Start()
+	{
+		
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	
+	void FixedUpdate()
+	{   
+		float xInput = Input.GetAxis("Horizontal");
+		float yInput = Input.GetAxis("Vertical");
+
+		Vector2 direction = new Vector2(xInput, yInput).normalized;
+		body.velocity = direction*speed;
+	}
 }
