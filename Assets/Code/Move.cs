874d1b8db17d115b6518e51f5aafd0304acc4a0e
verylowpower <doudqwer@gmail.com>
2024-08-12 16:35:19 +0700

    thêm nhảy và sửa 1 vài lỗi liên quan Ghost jump bằng cách sử dụng Coyote Time

--- Assets/Code/Move.cs
+++ Assets/Code/Move.cs
@@ -5,49 +5,83 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {	
-	[SerializeField] float speed;
-	[SerializeField] float drag;
-	bool grounded;
+	public float speed;
+	[Range(0f, 1f)]
+	public float groundDecay;
+	public float jumpForce;
+	public bool grounded;
+	public BoxCollider2D groundCheck;
+	public LayerMask groundMask;
 	public Rigidbody2D body;
+	float xInput;
+	float yInput;
+	private float coyoteTime = 0.1f; // thời gian chờ
+	private float coyoteTimeCounter;
+
+	
 	void Start()
 	{
 		
 	}
 
-	
 	void Update()
 	{   
-		float xInput = Input.GetAxis("Horizontal");
-		float yInput = Input.GetAxis("Vertical");
-
+		GetInput();
+		InputMove();
+		if (grounded)
+		{
+			coyoteTimeCounter = coyoteTime;
+		}
+		else
+		{
+			coyoteTimeCounter -= Time.deltaTime;
+		}
+		
+	}
+	
+	void FixedUpdate()
+	{	
+		CheckGround();
+		ApplyFriction();
+	}
+	
+	void GetInput()
+	{
+		xInput = Input.GetAxis("Horizontal");
+		yInput = Input.GetAxis("Vertical");
+		//Vector2 direction = new Vector2(xInput, yInput).normalized;
+		//body.velocity = direction*speed;
+	}
+	
+	void InputMove()
+	{
 		if (Mathf.Abs(xInput)>0)
 		{
 			body.velocity = new Vector2(xInput*speed,body.velocity.y);
 		}
 		
-		if (Mathf.Abs(yInput)>0)
+		if (yInput > 0.1f && coyoteTimeCounter > 0f) // Sử dụng coyote time
 		{
-			body.velocity = new Vector2(body.velocity.x,yInput*speed);
+			body.velocity = new Vector2(body.velocity.x, jumpForce);
+			body.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
 		}
-		
-		//Vector2 direction = new Vector2(xInput, yInput).normalized;
-		//body.velocity = direction*speed;
 	}
 	
-	void FixedUpdate()
-	{	
-		if(grounded)
+	void ApplyFriction()
+	{
+		if(grounded && xInput==0 && yInput==0 )
 		{
-			body.velocity *= drag;	
+			body.velocity *= groundDecay;	
+			//Debug.Log("On_Ground");
 		}
-		
+		// else if(!grounded)
+		// {
+		// 	Debug.Log("Not Ground");
+		// }
 	}
 	
 	void CheckGround()
-	{
-		if(gameObject.CompareTag("Ground"))
-		{
-			
-		}
+	{	
+		grounded = Physics2D.OverlapArea(groundCheck.bounds.min,groundCheck.bounds.max,groundMask) != null;		
 	}
 }
