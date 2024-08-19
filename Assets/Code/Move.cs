66c2227329e0b9aa3a4bf74f02eeece16f514e39
verylowpower <doudqwer@gmail.com>
2024-08-19 22:04:48 +0700

    fix conflict

--- Assets/Code/Move.cs
+++ Assets/Code/Move.cs
@@ -1,87 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
-public class Move : MonoBehaviour
-{	
+public class Test : MonoBehaviour
+{
+	float horizontal;
 	public float speed;
-	[Range(0f, 1f)]
-	public float groundDecay;
 	public float jumpForce;
-	public bool grounded;
-	public BoxCollider2D groundCheck;
-	public LayerMask groundMask;
-	public Rigidbody2D body;
-	float xInput;
-	float yInput;
-	private float coyoteTime = 0.1f; // thời gian chờ
-	private float coyoteTimeCounter;
+	public bool isFacingRight = true;
 
-	
-	void Start()
-	{
-		
-	}
+	[SerializeField] Rigidbody2D body;
+	[SerializeField] Transform groundCheck;
+	[SerializeField] LayerMask groundLayer;
 
 	void Update()
-	{   
-		GetInput();
-		InputMove();
-		if (grounded)
-		{
-			coyoteTimeCounter = coyoteTime;
-		}
-		else
-		{
-			coyoteTimeCounter -= Time.deltaTime;
-		}
-		
-	}
-	
-	void FixedUpdate()
-	{	
-		CheckGround();
-		ApplyFriction();
-	}
-	
-	void GetInput()
 	{
-		xInput = Input.GetAxis("Horizontal");
-		yInput = Input.GetAxis("Vertical");
-		//Vector2 direction = new Vector2(xInput, yInput).normalized;
-		//body.velocity = direction*speed;
+		horizontal = Input.GetAxisRaw("Horizontal");
+		Jump();
+		Flip();
 	}
-	
-	void InputMove()
+
+	private void LateUpdate()
 	{
-		if (Mathf.Abs(xInput)>0)
-		{
-			body.velocity = new Vector2(xInput*speed,body.velocity.y);
-		}
-		
-		if (yInput > 0.1f && coyoteTimeCounter > 0f) // Sử dụng coyote time
+		body.velocity = new Vector2(horizontal * speed, body.velocity.y);
+	}
+
+	private bool isGrounded()
+	{
+		return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+	}
+
+
+	public void Jump()
+	{
+		if (Input.GetButtonDown("Jump") && isGrounded())
 		{
 			body.velocity = new Vector2(body.velocity.x, jumpForce);
-			body.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
 		}
-	}
-	
-	void ApplyFriction()
-	{
-		if(grounded && xInput==0 && yInput==0 )
+
+		if (Input.GetButtonUp("Jump") && body.velocity.y > 0f)
 		{
-			body.velocity *= groundDecay;	
-			//Debug.Log("On_Ground");
+			body.velocity = new Vector2(body.velocity.x, body.velocity.y * 0.5f);
 		}
-		// else if(!grounded)
-		// {
-		// 	Debug.Log("Not Ground");
-		// }
 	}
-	
-	void CheckGround()
-	{	
-		grounded = Physics2D.OverlapArea(groundCheck.bounds.min,groundCheck.bounds.max,groundMask) != null;		
+
+
+	private void Flip()
+	{
+		if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+		{
+			isFacingRight = false;
+			Vector3 localScale = transform.localScale;
+			localScale.x *= -1f;
+			transform.localScale = localScale;
+		}
 	}
+
 }
